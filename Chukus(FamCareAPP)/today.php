aa6d1dbc1cc66a13cd04fa692c13d493ef51407b
Hardik Hajela <69126651+HardikHajela@users.noreply.github.com>
2022-12-12 23:38:33 -0500

    Update today.php

--- Chukus(FamCareAPP)/today.php
+++ Chukus(FamCareAPP)/today.php
@@ -5,6 +5,12 @@
 	<!--title-->
     	<title>Today</title>
 	  
+	<!--meta tags-->
+    	<meta charset="UTF-8">
+    	<meta name="author" content="Hardik Hajela">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+		
 	
   </head>
   
